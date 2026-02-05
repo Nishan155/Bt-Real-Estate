2c9a6b3235f98e29b11ea653e5ce4256db7c3b56
Nishan <nishansapkota155@example.com>
2026-02-05 14:02:47 +0545

    Accounts and Authentications

--- btre/settings.py
+++ btre/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'listings.apps.ListingsConfig',
     'realtors.apps.RealtorsConfig',
     'django.contrib.humanize',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
@@ -131,3 +132,10 @@ STATICFILES_DIRS=[
 #Media Folder settings
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 MEDIA_URL='/media/'
+
+
+#Messages
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR:'danger'
+}
